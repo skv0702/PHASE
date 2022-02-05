14873eb9242eeebd190303e566acd8543f3be808
Shubham kumar vivek <shubham.sk.kumar007@gmail.com>
2022-02-05 14:46:46 +0530

    emailvalidiate commited

---
+++ PHASE 1/Practise Project/EmailValidiate/src/EmailValidiate/EmailValidiate.java
@@ -0,0 +1,34 @@
+package EmailValidiate;
+
+import java.util.ArrayList;
+import java.util.regex.Pattern;
+
+public class EmailValidiate {
+    public static boolean isValid(String email)
+    {
+        String emailRegex = "^[a-zA-Z0-9_+&*-]+(?:\\."+
+                            "[a-zA-Z0-9_+&*-]+)*@" +
+                            "(?:[a-zA-Z0-9-]+\\.)+[a-z" +
+                            "A-Z]{2,7}$";
+                              
+        Pattern pat = Pattern.compile(emailRegex);
+        if (email == null)
+            return false;
+        return pat.matcher(email).matches();
+    }
+  
+    public static void main(String[] args)
+    {
+        ArrayList<String> address = new ArrayList<>();
+            
+          address.add("shubham.kumar.vivek@gmail.com");
+          address.add("shubham kumar vivek.com");
+            
+        for(String i : address){
+            if (isValid(i))
+                System.out.println(i + " - Yes");
+            else
+                System.out.println(i + " - No");
+        }
+    }
+}
