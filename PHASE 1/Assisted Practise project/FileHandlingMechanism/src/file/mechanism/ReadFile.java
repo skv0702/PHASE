ef649963a5be69e860c521b6c99e2ee2d2d675a4
Shubham kumar vivek <shubham.sk.kumar007@gmail.com>
2022-02-08 20:49:28 +0530

    FH mechanism

---
+++ PHASE 1/Assisted Practise project/FileHandlingMechanism/src/file/mechanism/ReadFile.java
@@ -0,0 +1,36 @@
+package file.mechanism;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
+
+public class ReadFile { 
+	  public static List<String> readFileInList(String fileName) 
+	  { 
+	  
+	    List<String> lines = Collections.emptyList(); 
+	    try
+	    { 
+	      lines = 
+	       Files.readAllLines(Paths.get(fileName), StandardCharsets.UTF_8); 
+	    } 
+	  
+	    catch (IOException e) 
+	    { 
+	      e.printStackTrace(); 
+	    } 
+	    return lines; 
+	  } 
+	  public static void main(String[] args) 
+	  { 
+	    List l = readFileInList("c://temp//testFile2.txt"); 
+	  
+	    Iterator<String> itr = l.iterator(); 
+	    while (itr.hasNext()) 
+	      System.out.println(itr.next()); 
+	  } 
+	}
