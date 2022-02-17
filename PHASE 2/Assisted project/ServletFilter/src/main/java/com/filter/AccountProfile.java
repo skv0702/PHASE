f6a81b78fa2cadf04c85248a3e79e3e6e8fee713
Shubham kumar vivek <shubham.sk.kumar007@gmail.com>
2022-02-17 22:40:37 +0530

    servlet filter commited

---
+++ PHASE 2/Assisted project/ServletFilter/src/main/java/com/filter/AccountProfile.java
@@ -0,0 +1,39 @@
+package com.filter;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/AccountProfile")
+public class AccountProfile extends HttpServlet {
+        private static final long serialVersionUID = 1L;
+       
+    /**
+* @see HttpServlet#HttpServlet()
+*/
+    public AccountProfile() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+        /**
+         * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+         */
+        protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+                // TODO Auto-generated method stub
+                response.getWriter().write("This is the Account profile of the filter");
+        }
+
+        /**
+         * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+         */
+        protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+                // TODO Auto-generated method stub
+                doGet(request, response);
+        }
+
+}
