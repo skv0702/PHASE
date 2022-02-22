b9914f97aef29625d2c20a27ab3ee17d5ce1e4e5
Shubham kumar vivek <shubham.sk.kumar007@gmail.com>
2022-02-22 16:49:36 +0530

    commited product

---
+++ PHASE 2/Practise Project/Product/src/main/java/com/product/ProductDetails.java
@@ -0,0 +1,28 @@
+package com.product;
+
+public class ProductDetails {
+
+	int Id;
+	String Name;
+	float Price;
+	public int getId() {
+		return Id;
+	}
+	public void setId(int id) {
+		this.Id = id;
+	}
+	public String getName() {
+		return Name;
+	}
+	public void setName(String name) {
+		this.Name = name;
+	}
+	public float getPrice() {
+		return Price;
+	}
+	public void setPrice(float price) {
+		Price = price;
+	}
+	
+	
+}
---
+++ PHASE 2/Practise Project/Product/src/main/java/com/product/productSerrvlet.java
@@ -0,0 +1,69 @@
+package com.product;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class productSerrvlet
+ */
+public class productSerrvlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public productSerrvlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		 PrintWriter pw=response.getWriter();
+		 ProductDetails pd=new ProductDetails();
+		 pd.setId(Integer.parseInt(request.getParameter("id")));
+ 		 pw.println("<html><body>");  
+ 		 response.setContentType("text/html");
+ 		
+
+      		try {
+      			Connection conn =DBconnection.dbConn();
+      		 String sql="select * from Product where id=(?)";
+      		 PreparedStatement ps=conn.prepareStatement(sql);
+      		 ps.setInt(1,pd.getId());
+      		 ResultSet rs=ps.executeQuery();
+      		 RequestDispatcher rd=request.getRequestDispatcher("index.html");
+      		 rd.include(request, response);
+     		
+      		 pw.println("<center><table border=1 width=14% height=10%>");  
+      		 pw.println("<tr><th>Product Id</th><th>Product Name</th><th>Price</th>"); 
+      		 while(rs.next()) {
+
+      			 pw.println("<tr><td>" + rs.getInt(1)+ "</td><td>" + rs.getString(2) +"</td><td>"+ rs.getInt(3)+"</td></tr></center>");   
+      			 
+      		 }
+      		 pw.println("</table>");  
+      		 pw.println("</html></body>");  
+      		 //rd.include(request, response);
+      		 
+      	 } catch (SQLException e) {
+      		 // TODO Auto-generated catch block
+      		 e.printStackTrace();
+      	 } catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}}
+
