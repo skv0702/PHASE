6fd19a4a178c881e8784dceb08e42aff3e846fe9
Shubham kumar vivek <shubham.sk.kumar007@gmail.com>
2022-02-26 18:39:12 +0530

    commited spring project phase2

---
+++ PHASE 2/Assisted project/SpringHibernateWeb/src/main/java/SpringHibernateWeb/SpringHibernateWeb/App.java
@@ -0,0 +1,13 @@
+package SpringHibernateWeb.SpringHibernateWeb;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
