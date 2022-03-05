370570a9f37d9d6fd10a1c814033060326c9c0a7
Shubham kumar vivek <shubham.sk.kumar007@gmail.com>
2022-03-05 19:16:18 +0530

    commited spring kafka

---
+++ PHASE 3/Assisted project/SpringKafka/src/main/java/com/ecommerce/SpringKafkaApplication.java
@@ -0,0 +1,27 @@
+package com.ecommerce;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.core.KafkaTemplate;
+
+@SpringBootApplication
+public class SpringKafkaApplication {
+
+        @Autowired
+           private KafkaTemplate<String, String> kafkaTemplate;
+
+        public static void main(String[] args) {
+                SpringApplication.run(SpringKafkaApplication.class, args);
+                
+        }
+        
+
+        
+           @KafkaListener(topics = "ecommerce", groupId = "group-id")
+           public void listen(String message) {
+              System.out.println("Received Message in group - group-id: " + message);
+           }
+          
+}
